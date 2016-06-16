d34153598eb244e48b0da9a9f9d740dd6a180273
salmaanrizvi <sar228@cornell.edu>
2016-06-16 15:58:11 -0400

    all tests pass epochly

---
+++ EpochButton/FISEpochViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISEpochViewController.h
+//  EpochButton
+//
+//  Created by Salmaan Rizvi on 6/16/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISEpochViewController : UIViewController
+
+@end
